a1bf43890397e5ddd48207a54abfdc997dbbc0db
Jonathan Vogt <jonathan.vogt@allcloud.io>
2022-06-20 13:49:23 +0200

    update middleware handler

--- internal/http/webhook/middleware.go
+++ internal/http/webhook/middleware.go
@@ -4,11 +4,12 @@ import (
 	"net/http"
 
 	"github.com/slok/go-http-metrics/middleware"
+	middlewarestd "github.com/slok/go-http-metrics/middleware/std"
 )
 
 // measuredHandler wraps a handler and measures the request handled
 // by this handler.
 func (h handler) measuredHandler(next http.Handler) http.Handler {
 	mdlw := middleware.New(middleware.Config{Recorder: h.metrics})
-	return mdlw.Handler("", next)
+	return middlewarestd.Handler("", mdlw, next)
 }
